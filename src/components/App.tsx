d5c5ba313acde00561650768e89e8d298da5471b
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-05-21 19:25:54 +0530

    feat: added sass and the title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,9 @@
+import Header from './Header';
+
 function App() {
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+	return (
+		<h1 className="ribbon">
+			<strong className="ribbon-content">
+				PERIODIC TABLE
+				&nbsp;<span className="ribbon-of">of</span>&nbsp;
+				KGPIAN SOCIETIES
+			</strong>
+		</h1>
+	);
+}
+
+export default Header;
