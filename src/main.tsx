f1ac3dceacbca036c7428d8d948c97619d48c659
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-06-10 00:26:25 +0530

    fix: removed remnants of react-tooltip

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './styles/index.scss'
-import 'react-tooltip/dist/react-tooltip.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
