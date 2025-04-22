e88611b88597d0955d5dadb1e96c9e6c989b457f
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2025-04-22 11:22:27 +0530

    feat: also on fotter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ function Footer() {
 		<footer className="footer">
 			<span>Maintained with <b>{"</>"}</b> by <a className="footer-highlight" href="https://github.com/metakgp">metaKGP</a> | Created with ❤️ by <a className="footer-highlight" href="https://www.facebook.com/scholarsavenue/">The Scholars' Avenue</a></span>
 			<span>Please discuss or contribute suggestions on our <a className="footer-highlight" href="https://github.com/metakgp/societytable">Github</a> or <a className="footer-highlight" href="https://slack.metakgp.org">Slack</a></span>
+			<span>If you want to add a society, report it <a className="footer-highlight" href="https://github.com/metakgp/societytable/issues/new?template=new_soc.yml">here</a> or on Slack.</span>
 		</footer>
 	);
 }
